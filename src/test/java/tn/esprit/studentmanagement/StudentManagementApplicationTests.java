4bbba20777a5f28e79fa3edf0486f8e1adbead34
Abir Ben Brahem <124564770+Abirbenbrahem-Git@users.noreply.github.com>
2025-11-27 15:31:42 +0100

    Add ActiveProfiles annotation for test configuration

--- src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
+++ src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
@@ -1,13 +1,16 @@
+StudentManagementApplicationTests.java: 
 package tn.esprit.studentmanagement;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
 
 @SpringBootTest
+@ActiveProfiles("test")
 class StudentManagementApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
