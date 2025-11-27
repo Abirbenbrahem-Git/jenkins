5cd3d7b5b45e8c955c8842fe662ee59127bf4987
Abir Ben Brahem <124564770+Abirbenbrahem-Git@users.noreply.github.com>
2025-11-27 18:44:33 +0100

    Remove unused ActiveProfiles annotation

--- src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
+++ src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
@@ -1,16 +1,13 @@
-StudentManagementApplicationTests.java: 
 package tn.esprit.studentmanagement;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-
 
 @SpringBootTest
-@ActiveProfiles("test")
 class StudentManagementApplicationTests {
 
     @Test
     void contextLoads() {
     }
+
 }
